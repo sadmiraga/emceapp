b6bb20832af463aac657f3171bb3fb2260dd6788
sadmiraga <sadmirvela@gmail.com>
2021-06-01 17:12:00 +0200

    seeder, adminLayout prije package

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -13,6 +13,8 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    <script src="{{ asset('js/admin/sidebar.js') }}" defer></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -21,28 +23,76 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
+
+
 <body>
     <div id="app">
 
-        <div class="sidebar">
 
-            <div class="sidebarContainer">
 
-                <button><i class="fa fa-bars"></i></button>
-                <img src="images/logos/emceLogo.jpg" class="sidebarLogo" width="200">
+
+
+        <div id="wrapper">
+
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                    <li class="sidebar-brand">
+                        <a href="#">
+                            Start Bootstrap
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#">Shortcuts</a>
+                    </li>
+                    <li>
+                        <a href="#">Overview</a>
+                    </li>
+                    <li>
+                        <a href="#">Events</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
             </div>
-            <i class="fas fa-bars"></i>
-            <ul>
-                <li>Popisi</li>
-                <li>Kelnerji</li>
-                <li>Pijače</li>
-                <li>Kategorije pijač</li>
-            </ul>
+            <!-- /#sidebar-wrapper -->
+
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1>Simple Sidebar</h1>
+                            <p>This template has a responsive menu toggling system. The menu will appear collapsed on
+                                smaller screens, and will appear non-collapsed on larger screens. When toggled using the
+                                button below, the menu will appear/disappear. On small screens, the page content will be
+                                pushed off canvas.</p>
+                            <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /#page-content-wrapper -->
 
         </div>
+        <!-- /#wrapper -->
 
 
-        <main class="py-4">
+
+        <main class="py-4 " style="margin-left:20%;">
+            <button class="btn btn-primary" onclick="closeSidebar()">CLOSE</button>
+            <button class="btn btn-primary" onclick="openSidebar()">OPEN</button>
             @yield('content')
         </main>
     </div>
