eb132892859776af045bfdf4fe0d6ca3af819e96
Sadmir <sadmirvela@gmail.com>
2021-12-04 22:05:29 +0100

    seeder

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -16,13 +16,10 @@ class userSeeder extends Seeder
     {
         DB::table('users')->delete();
         $users = array(
-            array('id' => 1, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uporabnik@gmail.com', 'password' => '$2y$10$.h5OFviwCBjyrNZMlw1d8.m1qPQ5sRXcOyvbEPMlsQddn//tB.n9.', 'type_id' => '1'),
-            array('id' => 2, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'kelner@gmail.com', 'password' => '$2y$10$.h5OFviwCBjyrNZMlw1d8.m1qPQ5sRXcOyvbEPMlsQddn//tB.n9.', 'type_id' => '2'),
-            array('id' => 3, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uperativc@gmail.com', 'password' => '$2y$10$.h5OFviwCBjyrNZMlw1d8.m1qPQ5sRXcOyvbEPMlsQddn//tB.n9.', 'type_id' => '3'),
-            array('id' => 4, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'direktor@gmail.com', 'password' => '$2y$10$.h5OFviwCBjyrNZMlw1d8.m1qPQ5sRXcOyvbEPMlsQddn//tB.n9.', 'type_id' => '4'),
-
-
-
+            array('id' => 1, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uporabnik@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '1'),
+            array('id' => 2, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'kelner@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '2'),
+            array('id' => 3, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uperativc@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '3'),
+            array('id' => 4, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'direktor@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '4'),
         );
         DB::table('users')->insert($users);
     }
