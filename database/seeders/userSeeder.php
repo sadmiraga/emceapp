c0d882a83c256ab387c058b660fadee8d42e0759
Sadmir <sadmirvela@gmail.com>
2021-12-05 22:12:00 +0100

    cover to contain

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -18,7 +18,7 @@ class userSeeder extends Seeder
         $users = array(
             array('id' => 1, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uporabnik@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '1'),
             array('id' => 2, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'kelner@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '2'),
-            array('id' => 3, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'uperativc@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '3'),
+            array('id' => 3, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'pperativc@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '3'),
             array('id' => 4, 'firstName' => 'Sadmir', 'lastName' => 'Hasanic', 'email' => 'direktor@gmail.com', 'password' => '$2y$10$2WYJNk8zfECoJzC1t4ziIOSpZ0ciP2MDPH3N0nfTptx1DXNNBRff6', 'type_id' => '4'),
         );
         DB::table('users')->insert($users);
